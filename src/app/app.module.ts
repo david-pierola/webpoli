04c4e7e1d0138d4e6142a27429f76f558ce5f10c
sim-mbj <56660292+sim-mbj@users.noreply.github.com>
2019-10-18 00:59:13 -0400

    add base styles and base menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { CoreModule } from "./core/core.module";
 import { HttpClientModule } from "@angular/common/http";
 import { HeaderComponent } from "./layouts/header/header.component";
 import { FooterComponent } from "./layouts/footer/footer.component";
+import { HeaderMenuComponent } from './menu/header-menu/header-menu.component';
+import { LeftMenuComponent } from './menu/left-menu/left-menu.component';
+import { HomeComponent } from './home/home.component';
+import { GeneralInformationComponent } from './unique-form/general-information/general-information.component';
 import { HomeComponent } from "./modules/home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, HeaderMenuComponent,
+    LeftMenuComponent,
+    HomeComponent,
+    GeneralInformationComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
+    
   providers: [],
   bootstrap: [AppComponent]
 })
