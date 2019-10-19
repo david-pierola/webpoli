a1219eaae4941abff3f992864771c41924338e38
sim-mbj <56660292+sim-mbj@users.noreply.github.com>
2019-10-19 00:37:52 -0400

    Add style for sidebar and events for toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"]
 })
 export class AppComponent {
-  title = 'test';
+  title = "SINARAP";
+  status: boolean = true;
+
+  toogleSidebar($event) {
+    this.status = $event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,25 @@ import { CoreModule } from "./core/core.module";
 import { HttpClientModule } from "@angular/common/http";
 import { HeaderComponent } from "./layouts/header/header.component";
 import { FooterComponent } from "./layouts/footer/footer.component";
-import { HeaderMenuComponent } from './menu/header-menu/header-menu.component';
-import { LeftMenuComponent } from './menu/left-menu/left-menu.component';
-import { GeneralInformationComponent } from './unique-form/general-information/general-information.component';
+import { HeaderMenuComponent } from "./menu/header-menu/header-menu.component";
+import { LeftMenuComponent } from "./menu/left-menu/left-menu.component";
+import { GeneralInformationComponent } from "./unique-form/general-information/general-information.component";
 import { HomeComponent } from "./modules/home/home.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, HeaderMenuComponent,
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    HeaderMenuComponent,
     LeftMenuComponent,
     HomeComponent,
-    GeneralInformationComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
-    
+    GeneralInformationComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule, FontAwesomeModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
